51c07da13c77bc667540253f5d5f3dc5ff078b3f
vivek <vivek@athenaeum.in>
2021-05-18 08:38:28 +0530

    done

--- Vehicle.Client/Controllers/VehiclesController.cs
+++ Vehicle.Client/Controllers/VehiclesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Google.Protobuf.WellKnownTypes;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VehicleGrpcService;
+using static VehicleGrpcService.VehicleServiceContract;
 
 namespace Vehicle.Client.Controllers
 {
@@ -11,16 +14,27 @@ namespace Vehicle.Client.Controllers
     [ApiController]
     public class VehiclesController : ControllerBase
     {
+        public readonly VehicleServiceContractClient _grpcClient;
+
+        public VehiclesController(VehicleServiceContractClient grpcClient)
+        {
+            _grpcClient = grpcClient;
+        }
+
         [HttpGet("random")]
         public async Task<IActionResult> GetRandom()
         {
-            return Ok();
+            return Ok(await _grpcClient.GetRandomRecommendationAsync(new Empty()));
         }
 
         [HttpGet("model")]
         public async Task<IActionResult> GetModel()
         {
-            return Ok();
+            return Ok(await _grpcClient.GetVehicleAsync(new VehicleRequest
+            {
+                Make = "Suzuki",
+                Model = "Baleno"
+            }));
         }
     }
 }
--- Vehicle.Client/Startup.cs
+++ Vehicle.Client/Startup.cs
@@ -19,6 +19,7 @@ namespace Vehicle.Client
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
             services.AddSingleton(typeof(VehicleServiceContractClient), sp =>
             {
                 var channel = GrpcChannel.ForAddress("https://localhost:5001");
@@ -38,10 +39,7 @@ namespace Vehicle.Client
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
