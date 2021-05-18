5426896a0d69805974e4a69f3d340a0a6b21deae
vivek <vivek@athenaeum.in>
2021-05-18 08:19:12 +0530

    done

---
+++ Vehicle.Client/Controllers/VehiclesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicle.Client.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        [HttpGet("random")]
+        public async Task<IActionResult> GetRandom()
+        {
+            return Ok();
+        }
+
+        [HttpGet("model")]
+        public async Task<IActionResult> GetModel()
+        {
+            return Ok();
+        }
+    }
+}
